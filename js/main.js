3346563497ef4451c04f487a6a56d29873e29cf8
Md Suzayet Hossan <md.suzayet.377@gmail.com>
2022-07-13 07:39:05 +0600

    second

--- js/main.js
+++ js/main.js
@@ -28,5 +28,42 @@ $('.engineer-main').slick({
     arrows: true,
     prevArrow: '.slick-prev',
     nextArrow: '.slick-next',
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+
+        }
+    },
+
+    {
+        breakpoint: 780,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+        }
+    },
+    {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 3
+        }
+    },
+    {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+        }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+]
   });
 });
